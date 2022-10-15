5a39f4e3ef9fa6d66d287e8f2b254a8be9997540
Susan Winters <susanwinters214@gmail.com>
2022-10-15 16:06:22 -0500

    game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lets play a game! Guess a number between 1-1000.");
+
+
+            var r = new Random();  // set fav num to random so that game can be played multiple times
+            var favNumber = r.Next(1, 1000);
+            bool isWinner = false;
+
+            do
+            {
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput == favNumber)
+                {
+                    Console.WriteLine("You win!");
+                    isWinner = true;
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low! Guess again.");
+
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high! Guess again");
+                }
+            }
+            while (!isWinner);
+
+            Console.WriteLine($"The answer was {favNumber}!");
+
         }
     }
 }
